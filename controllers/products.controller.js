2330418d301629e7dd68ab02ab2a9ecd303ea788
truongson04 <dangson2376@gmail.com>
2025-08-21 15:25:47 +0700

    Create the layout for the product page

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,6 +1,10 @@
-module.exports.index=(req, res)=>{
+const Product = require("../models/product.models")
+module.exports.index= async(req, res)=>{
+const products =   await Product.find({});
+console.log(products);
     res.render("client/pages/products/index", {
-        pageTitle: "Product Page"
+        pageTitle: "Product Page",
+        products: products
     })
 
 

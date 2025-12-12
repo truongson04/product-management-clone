10a01b8ebcb2ed9df898a27e79aca8497afb7905
baodeptrai912 <dangson2376@gmail.com>
2025-12-13 01:19:46 +0700

    Config for deploy

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(bodyParser.urlencoded({extended:false}))
  app.use(cookieParser('keyboard cat'));
   app.use(session({ cookie: { maxAge: 60000 }}));
   app.use(flash());
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
-app.use(express.static("public"))
+app.use(express.static(`${__dirname}/public`))
 route(app);
 routeAdmin(app);
 

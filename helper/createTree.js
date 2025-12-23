68c498ca8966563ade9cedf23e61c2ff67b3918e
baodeptrai912 <dangson2376@gmail.com>
2025-12-23 16:10:50 +0700

    Fix the order of the product in the main admin page

--- helper/createTree.js
+++ helper/createTree.js
@@ -1,8 +1,11 @@
+let count=1;
 function createTree (arr, parentId=""){
        let tree=[];
         arr.forEach((items)=>{
             if(items.parent_id==parentId){
                 const newItem = items;
+                count++;
+                newItem.number=count;
                 const children = createTree(arr, items.id);
             if(children.length>0){
                 newItem.children=children;
@@ -14,6 +17,7 @@ function createTree (arr, parentId=""){
         return tree;
 }
 module.exports.createTree= (arr, parentId="")=>{
+        count=0;
        let tree=[];
         arr.forEach((items)=>{
             if(items.parent_id==parentId){
